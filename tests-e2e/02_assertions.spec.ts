2400f487c7d804dca4e18602e6a9d0e5a9501300
Nidhi Singh <nidhi.into.pgt@gmail.com>
2025-01-08 10:57:29 +0100

    update2

--- tests-e2e/02_assertions.spec.ts
+++ tests-e2e/02_assertions.spec.ts
@@ -2,7 +2,7 @@ import {expect, test} from '@playwright/test'
 test ("Assertions demo", async ({page,baseURL,browserName}) => {  //need not to write browser, context and new page
     //test.slow()
     //test.slow (browserName === 'firefox')
-    await page.goto(`$ {baseURL}`)
+    await page.goto(`${baseURL}`)
     //await page.locator("//input[@id='user-message']").fill("Hello World!!!")
     const messageText = "My message !!"
     const messagebox = page.locator("//input[@id='user-message']") //creates the object of the element
@@ -11,7 +11,7 @@ test ("Assertions demo", async ({page,baseURL,browserName}) => {  //need not to 
     await messagebox.fill(`${messageText}`)
     await page.waitForTimeout(2000)
 
-    //await context.clearCookies();
+ 
 
     await page.click("#showInput")
     //const displayedMsg = page.locator("id=message")
